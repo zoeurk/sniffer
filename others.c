6521b287f5075d1c76f5d790585f6aba68a57995
zoeurk <zoeurk@gmail.com>
2020-07-17 18:21:50 +0200

    update

--- others.c
+++ others.c
@@ -4,7 +4,7 @@
 
 #include "others.h"
 
-const char 			*argp_program_version = "sniffer-1.0";
+const char 			*argp_program_version = "sniffer-2.1";
 const char 			*argp_program_bug_address = "zoeurk@gmail.com";
 
 struct output		myoutput = {0, {'\0'}, 0, 0, 0, 0 ,0, 0, 0, 0, 0,
